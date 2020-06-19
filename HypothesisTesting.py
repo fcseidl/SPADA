78c4c818837f48f06a219f4d53af542fe2cdf459
fcseidl <fcseidl@umich.edu>
2020-06-19 11:34:01 -0400

    more scalable testing with conical hulls

--- HypothesisTesting.py
+++ HypothesisTesting.py
@@ -11,6 +11,7 @@ Tests to decide whether bulk and scRNA-seq datasets are joint.
 import numpy as np
 from sklearn.preprocessing import normalize
 from scipy.spatial import ConvexHull
+from scipy.optimize import linprog
 
 
 def HullContainment(X, Y):
@@ -41,23 +42,83 @@ def HullContainment(X, Y):
     return (vertices >= M).all()
 
 
+def fractionInsideCone(X, Y):
+    """
+    Count fraction of bulk RNA-seq profiles which are conical combinations of
+    single cell profiles.
+
+    Parameters
+    ----------
+    X : array, shape (N, M)
+        bulk data matrix
+    Y : array, shape (N, L)
+        single-cell data matrix
+
+    Returns
+    -------
+    Fraction of columns of X which are inside conical hull of columns of Y.
+    """
+    M = X.shape[1]
+    L = Y.shape[1]
+    count = 0
+    for j in range(M):  # see if X[:, j] is a convex combination of Y cols
+        c = np.zeros(L)
+        lp = linprog(c, A_eq=Y, b_eq=X[:, j])
+        if lp.success: count += 1
+    return count / M
+
+
+def fractionInsideHull(X, Y):
+    """
+    Count fraction of bulk RNA-seq profiles which are convex combinations of 
+    single cell profiles.
+
+    Parameters
+    ----------
+    X : array, shape (N, M)
+        bulk data matrix
+    Y : array, shape (N, L)
+        single-cell data matrix
+
+    Returns
+    -------
+    Fraction of normalized columns of X which are inside convex hull of 
+    normalized Y columns.
+    """
+    raise NotImplementedError()
+
+
 if __name__ == "__main__":
     import simulations as sims
     
-    N = 15
-    M = 8
-    L = 80
+    N = 100
+    M = 60
+    L = 800
     K = 4
-    alpha = [ 10 for _ in range(K) ]
+    alpha = [ 1e9 for _ in range(K) ]  # assumes symmetric Dirichlet prior
     A1 = sims.randomA(N, K)
     A2 = sims.randomA(N, K)
     X1 = sims.bulk(N, M, K, alpha, A=A1)
     X2 = sims.bulk(N, M, K, alpha, A=A2)
     Y1 = sims.true_single_cell(N, L, K, alpha, A=A1)
     
+    '''
+    print('Are X1 and Y1 joint? Expect 1.0, receive', 
+          fractionInsideHull(X1, Y1))
+    print('Are X2 and Y1 joint? Expect 0.0, receive', 
+          fractionInsideHull(X2, Y1))
+    '''
+    
+    print('Are X1 and Y1 joint? Expect 1.0, receive', 
+          fractionInsideCone(X1, Y1))
+    print('Are X2 and Y1 joint? Expect 0.0, receive', 
+          fractionInsideCone(X2, Y1))
+    
+    '''
     print('Are X1 and Y1 joint? Expect True, receive', 
           HullContainment(X1, Y1))
     print('Are X2 and Y1 joint? Expect False, receive', 
           HullContainment(X2, Y1))
+    '''
     
     
